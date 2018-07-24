63da9391dc563db8a41167aa5d782173d6917783
rvane <renevane@gmail.com>
2018-07-25 00:48:25 +0200

    Initial commit

---
+++ app/src/main/java/nl/renevane/employeeofthemonth/MainActivity.java
@@ -0,0 +1,67 @@
+package nl.renevane.employeeofthemonth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    // create fragment instances
+    private final PhotoFragment photoFragment = new PhotoFragment();
+    private final EditFragment editFragment = new EditFragment();
+    private final ShareFragment shareFragment = new ShareFragment();
+
+    // react to clicks on the nav buttons
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_photo:
+                    selectedFragment = photoFragment;
+                    break;
+                case R.id.nav_edit:
+                    selectedFragment = editFragment;
+                    break;
+                case R.id.nav_share:
+                    selectedFragment = shareFragment;
+                    break;
+            }
+
+            // show the selected fragment
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+
+            return true; // returning 'true' visually highlights the clicked navigation item
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set a reference to the bottom navigation view
+        // and pass the nav listener to it
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // on first run, show the Photo fragment
+        if (null == savedInstanceState) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, photoFragment)
+                    .commit();
+        }
+    }
+
+}
