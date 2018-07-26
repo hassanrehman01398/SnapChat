d72ff112be33e6edae6c7909cf55343dcbbe69cb
rvane <renevane@gmail.com>
2018-07-26 11:11:10 +0200

    Rename interface method name from 'onPhotoSaved' to 'onFileSaved'

--- app/src/main/java/nl/renevane/employeeofthemonth/MainActivity.java
+++ app/src/main/java/nl/renevane/employeeofthemonth/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
     }
 
     @Override
-    public void onPhotoSaved(String filePath) {
+    public void onFileSaved(String filePath) {
         editFragment.updatePath(filePath);
     }
 
