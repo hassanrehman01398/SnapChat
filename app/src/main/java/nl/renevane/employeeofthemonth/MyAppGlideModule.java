b5fcf441c36d9dce0d4e07ab0cf28e25ddb04fcb
rvane <renevane@gmail.com>
2018-07-28 22:04:22 +0200

    Set Glide bitmap format from default RGB_565 to ARGB_8888

--- app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
+++ app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
@@ -1,11 +1,25 @@
 package nl.renevane.employeeofthemonth;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 @GlideModule
 public final class MyAppGlideModule extends AppGlideModule {
 
-    // https://bumptech.github.io/glide/doc/generatedapi.html
+    // Needed for the Glide generated API
+    // See https://bumptech.github.io/glide/doc/generatedapi.html
+
+    @Override
+    public void applyOptions(@NonNull Context context, GlideBuilder builder) {
+        // Better image quality (and a bit more memory usage) than the default RGB_565
+        builder.setDefaultRequestOptions(new RequestOptions()
+                .format(DecodeFormat.PREFER_ARGB_8888));
+    }
 
 }
