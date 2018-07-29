6f61c11a9447d562fdb7908c034d9d8e5970c9e5
rvane <renevane@gmail.com>
2018-07-29 10:22:15 +0200

    Collapse class body and move comment

--- app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
+++ app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
@@ -3,9 +3,7 @@ package nl.renevane.employeeofthemonth;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+// More info on https://bumptech.github.io/glide/doc/generatedapi.html
+
 @GlideModule
-public final class MyAppGlideModule extends AppGlideModule {
-
-    // https://bumptech.github.io/glide/doc/generatedapi.html
-
-}
+public final class MyAppGlideModule extends AppGlideModule {}
