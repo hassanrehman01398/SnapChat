c7fab16c13bb94363d60e26c1992c831d11cd198
rvane <renevane@gmail.com>
2018-07-29 10:06:23 +0200

    Do not set default Glide options (see commit details)
    Glide v4 already defaults to ARGB_8888
    Check https://bumptech.github.io/glide/doc/migrating.html#decodeformat

--- app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
+++ app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
@@ -1,25 +1,11 @@
 package nl.renevane.employeeofthemonth;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-
-import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
-import com.bumptech.glide.request.RequestOptions;
 
 @GlideModule
 public final class MyAppGlideModule extends AppGlideModule {
 
-    // Needed for the Glide generated API
-    // See https://bumptech.github.io/glide/doc/generatedapi.html
-
-    @Override
-    public void applyOptions(@NonNull Context context, GlideBuilder builder) {
-        // Better image quality (and a bit more memory usage) than the default RGB_565
-        builder.setDefaultRequestOptions(new RequestOptions()
-                .format(DecodeFormat.PREFER_ARGB_8888));
-    }
+    // https://bumptech.github.io/glide/doc/generatedapi.html
 
 }
