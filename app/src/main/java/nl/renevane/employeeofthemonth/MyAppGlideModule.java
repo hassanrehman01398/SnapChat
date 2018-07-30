6a6a1a875fea36b6fc1b2e569aa5119ac9aadd3e
rvane <renevane@gmail.com>
2018-07-30 12:36:37 +0200

    Edit comment

--- app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
+++ app/src/main/java/nl/renevane/employeeofthemonth/MyAppGlideModule.java
@@ -3,6 +3,7 @@ package nl.renevane.employeeofthemonth;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+// This class enables Glide's generated API
 // More info on https://bumptech.github.io/glide/doc/generatedapi.html
 
 @GlideModule
